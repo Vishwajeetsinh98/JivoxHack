6e38738eb92e1e235173ada9c51209ba8acc96cc
TechDetective <vishwajeetsinhjadeja4@gmail.com>
2016-08-06 20:00:00 +0530

    Added Sex Filters

--- routes/govt.js
+++ routes/govt.js
@@ -21,17 +21,21 @@ router.post('/addProduct', function(req,res){
   var location2 = req.body.location2;
   var location3 = req.body.location3;
   var location4 = req.body.location4;
+  var maleFilter = req.body.male;
+  var femaleFilter = req.body.female;
   var data = {
     isGovt: true,
     campaignName: campaignName,
-    ageLower : ageLower,
-    ageHigher : ageHigher,
-    incomeLower : incomeLower,
-    incomeHigher : incomeHigher,
-    location1 : location1,
-    location2 : location2,
-    location3 : location3,
-    location4 : location4
+    ageLower: ageLower,
+    ageHigher: ageHigher,
+    incomeLower: incomeLower,
+    incomeHigher: incomeHigher,
+    location1: location1,
+    location2: location2,
+    location3: location3,
+    location4: location4,
+    maleFilter: maleFilter,
+    femaleFilter: femaleFilter
   };
   res.json(data);
 });
